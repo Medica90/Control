99abffd2e3c59d72f52acc47d6e66ffd0b5c55e8
maria <smolkina90@yandex.ru>
2022-12-27 18:17:23 +0500

    done

--- FinalProg.cs
+++ FinalProg.cs
@@ -1,1 +1,22 @@
 ﻿// Итоговая проверочная работа по первому блоку обучения на программе разработчик.
+ 
+string[] array =
+{
+    "hello",
+    "2",
+    "world",
+    ":-)"
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
